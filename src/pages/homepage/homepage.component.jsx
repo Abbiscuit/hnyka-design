777ecbcc13055c48ada357eaa6ed45d27261f3a4
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-19 18:40:19 +0900

    Setup basic packages and structure

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <section className="homepage">
+      <p>catalogue from homepage-component</p>
+    </section>
+  );
+};
+
+export default HomePage;
