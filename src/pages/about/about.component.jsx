3b7ef7e75ddda0414089f436b756512f12b66b05
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-19 22:26:55 +0900

    Update about page

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -6,6 +6,18 @@ const About = () => {
   return (
     <section className="about">
       <p>Hnyka Storeへようこそ！</p>
+      <p>
+        「毎日のちょっとした瞬間に、はなやかな気持ちになれたら…」
+        <br /> そんなコンセプトでモノづくりをしています。
+      </p>
+      <a
+        href="https://www.instagram.com/hnyka_abbiscuit/"
+        style={{
+          borderBottom: '3px solid pink'
+        }}
+      >
+        instagramアカウントはこちら
+      </a>
     </section>
   );
 };
