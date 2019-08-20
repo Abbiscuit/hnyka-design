dce63eb7e473878a10a051efce3012a729b7c5c6
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-21 07:53:23 +0900

    Update product_data

--- src/components/catalogue/catalogue.component.jsx
+++ src/components/catalogue/catalogue.component.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import './catalogue.styles.scss';
 
+import { product_data } from '../../product-data';
+
 const Catalogue = () => {
   return (
     <section className="catalogue">
+      {/* {
+        shop_data.map.....
+      } */}
       <div className="item-image image1" />
       <div className="item-image image2" />
       <div className="item-image image3" />
--- src/product-data.js
+++ src/product-data.js
@@ -8,43 +8,53 @@ export const product_data = [
         id: 1,
         name: 'Twisted Wire',
         price: 2000,
-        imageUrl: 'https://i.ibb.co/sRTg6Bh/earing.jpg'
+        imageUrl: 'https://i.ibb.co/sRTg6Bh/earing.jpg',
+        description:
+          '繊細なワイヤーで作る小枝アクセサリー。パールビースがアクセントです。'
       },
       {
         id: 2,
         name: 'Teardrop',
         price: 1300,
-        imageUrl: 'https://i.ibb.co/MG6RnNX/earing2.jpg'
+        imageUrl: 'https://i.ibb.co/MG6RnNX/earing2.jpg',
+        description: 'ドロップビーズとパールのピアス。'
       },
       {
         id: 3,
         name: 'Red Fruit',
         price: 1200,
-        imageUrl: 'https://i.ibb.co/W3y9nGd/earing3.jpg'
+        imageUrl: 'https://i.ibb.co/W3y9nGd/earing3.jpg',
+        description: '赤いフロッキービーズがポイントのアクセサリーです。'
       },
       {
         id: 4,
         name: 'Gold Flower',
         price: 1400,
-        imageUrl: 'https://i.ibb.co/SQx9WVP/earing4.jpg'
+        imageUrl: 'https://i.ibb.co/SQx9WVP/earing4.jpg',
+        description:
+          'ゴールドのフラワーパーツを使用し作られた、ノンホールタイプのピアスです。'
       },
       {
         id: 5,
         name: 'Pearl & Flower',
         price: 1200,
-        imageUrl: 'https://i.ibb.co/cKBcDpZ/earing5.jpg'
+        imageUrl: 'https://i.ibb.co/cKBcDpZ/earing5.jpg',
+        description: '大きなパールビーズがアクセントのピアスです。'
       },
       {
         id: 6,
         name: 'Teardrop Summer',
         price: 1400,
-        imageUrl: 'https://i.ibb.co/Mnwcghk/earing6.jpg'
+        imageUrl: 'https://i.ibb.co/Mnwcghk/earing6.jpg',
+        description:
+          'アメリカンピアス（チェーンピアス）をベースに、夏らしいストーンをほどこしたピアスです。'
       },
       {
         id: 7,
         name: 'Sakura',
         price: 1600,
-        imageUrl: 'https://i.ibb.co/gzZ8qky/sakura-earing.jpg'
+        imageUrl: 'https://i.ibb.co/gzZ8qky/sakura-earing.jpg',
+        description: '桜の花をイメージしたピアスです。'
       }
     ]
   },
@@ -57,19 +67,23 @@ export const product_data = [
         id: 1,
         name: 'Fur Bag',
         price: 2000,
-        imageUrl: 'https://i.ibb.co/hYCttMp/fur-bag.jpg'
+        imageUrl: 'https://i.ibb.co/hYCttMp/fur-bag.jpg',
+        description:
+          'フェイクファーを使ったがま口のカバンです。モコモコした素材が大人気。'
       },
       {
         id: 2,
         name: "Granny's",
         price: 2300,
-        imageUrl: 'https://i.ibb.co/ccH4Hng/granny-bag.jpg'
+        imageUrl: 'https://i.ibb.co/ccH4Hng/granny-bag.jpg',
+        description: '丸いフォルムが特徴的なグラニーバッグです。'
       },
       {
         id: 3,
         name: 'Gama',
         price: 2000,
-        imageUrl: 'https://i.ibb.co/xSc5j4g/gama.jpg'
+        imageUrl: 'https://i.ibb.co/xSc5j4g/gama.jpg',
+        description: 'レトロでかわいいがま口財布です。'
       }
     ]
   }
